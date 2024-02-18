ef8c3cd1141b72d0bcff6852d5dcebded8a6cf3a
GrayB <90840369+GrayBurford@users.noreply.github.com>
2024-02-17 23:09:49 -0500

    Create script.js

---
+++ Sliding.Window/script.js
@@ -0,0 +1,19 @@
+// Sliding window
+function fn (arr) {
+    let left = 0;
+    let ans = 0;
+    let curr = 0;
+
+    for (let right = 0; right < arr.length; right++) {
+        // do logic here to add arr[right] to curr
+
+        while (WINDOW_CONDITION_BROKEN) {
+            // remove arr[left] from curr
+            left++;
+        }
+
+        // update ans
+    }
+
+    return ans;
+}
