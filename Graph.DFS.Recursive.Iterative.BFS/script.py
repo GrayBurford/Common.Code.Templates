87a985c7785695e0e18c5524c22473b5a902a061
GrayB <90840369+GrayBurford@users.noreply.github.com>
2024-02-19 11:15:55 -0500

    Create script.py

---
+++ Graph.DFS.Recursive.Iterative.BFS/script.py
@@ -0,0 +1,52 @@
+# For the graph templates, assume the nodes are numbered from 0 to n - 1 and the graph is given as an adjacency list. Depending on the problem, you may need to convert the input into an equivalent adjacency list before using the templates.
+
+# Graph: DFS (recursive)
+def fn(graph):
+    def dfs(node):
+        ans = 0
+        # do some logic
+        for neighbor in graph[node]:
+            if neighbor not in seen:
+                seen.add(neighbor)
+                ans += dfs(neighbor)
+        
+        return ans
+
+    seen = {START_NODE}
+    return dfs(START_NODE)
+
+
+# Graph: DFS (iterative)
+def fn(graph):
+    stack = [START_NODE]
+    seen = {START_NODE}
+    ans = 0
+
+    while stack:
+        node = stack.pop()
+        # do some logic
+        for neighbor in graph[node]:
+            if neighbor not in seen:
+                seen.add(neighbor)
+                stack.append(neighbor)
+    
+    return ans
+
+
+# Graph: BFS
+from collections import deque
+
+def fn(graph):
+    queue = deque([START_NODE])
+    seen = {START_NODE}
+    ans = 0
+
+    while queue:
+        node = queue.popleft()
+        # do some logic
+        for neighbor in graph[node]:
+            if neighbor not in seen:
+                seen.add(neighbor)
+                queue.append(neighbor)
+    
+    return ans
