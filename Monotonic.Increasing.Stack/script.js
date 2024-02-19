76cfaec0b2f0dcef2b39737ec6cff8a1c1a49b23
GrayB <90840369+GrayBurford@users.noreply.github.com>
2024-02-18 21:11:48 -0500

    Create script.js

---
+++ Monotonic.Increasing.Stack/script.js
@@ -0,0 +1,19 @@
+
+function fn (arr) {
+    let stack = [];
+    let ans = 0;
+
+    for (const num of arr) {
+        // for monotonic decreasing, just flip the > to <
+        while (stack.length && stack[stack.length - 1] > num) {
+            // do logic
+            stack.pop();
+        }
+
+        stack.push(num);
+    }
+
+    return ans;
+}
+
+// The same logic can be applied to maintain a monotonic queue
