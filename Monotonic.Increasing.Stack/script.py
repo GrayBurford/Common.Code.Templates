963ec3d036b63d03a4be3d612cbb3faf1ae4ad94
GrayB <90840369+GrayBurford@users.noreply.github.com>
2024-02-18 21:12:15 -0500

    Create script.py

---
+++ Monotonic.Increasing.Stack/script.py
@@ -0,0 +1,14 @@
+def fn(arr):
+    stack = []
+    ans = 0
+
+    for num in arr:
+        # for monotonic decreasing, just flip the > to <
+        while stack and stack[-1] > num:
+            # do logic
+            stack.pop()
+        stack.append(num)
+    
+    return ans
+
+# The same logic can be applied to maintain a monotonic queue
