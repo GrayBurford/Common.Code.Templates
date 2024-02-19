696d5063ee9fdc60a493e828fb346ba8958fd77b
GrayB <90840369+GrayBurford@users.noreply.github.com>
2024-02-18 21:17:13 -0500

    Create script.py

---
+++ Binary.Tree.DFS.BFS.Recursive.And.Iterative/script.py
@@ -0,0 +1,49 @@
+#  Binary Tree: DFS (recursive)
+def dfs(root):
+    if not root:
+        return
+    
+    ans = 0
+
+    # do logic
+    dfs(root.left)
+    dfs(root.right)
+    return ans
+
+
+# Binary Tree: DFS (iterative)
+def dfs(root):
+    stack = [root]
+    ans = 0
+
+    while stack:
+        node = stack.pop()
+        # do logic
+        if node.left:
+            stack.append(node.left)
+        if node.right:
+            stack.append(node.right)
+
+    return ans
+  
+
+# Binary Tree: BFS
+from collections import deque
+
+def fn(root):
+    queue = deque([root])
+    ans = 0
+
+    while queue:
+        current_length = len(queue)
+        # do logic for current level
+
+        for _ in range(current_length):
+            node = queue.popleft()
+            # do logic
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+
+    return ans
